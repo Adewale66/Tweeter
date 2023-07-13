9ef98de3261b4f847b0f5d4017b4221912bd1642
adewale <adewalekujore6@gmail.com>
2023-07-13 09:38:21 +0100

    finished build

--- client/src/components/Tweet/Comments/Comments.tsx
+++ client/src/components/Tweet/Comments/Comments.tsx
@@ -1,12 +1,14 @@
 import { Box, Avatar, Group, Text, Stack, Flex } from "@mantine/core";
 import useStylesComment from "./styles";
 import getTimeFormat from "../utils/getTime";
+import { Link } from "react-router-dom";
 
 interface CommentProps {
   comment: string;
   madeBy: {
     username: string;
     profileimage: string;
+    name: string;
   };
   createdAt: string;
 }
@@ -21,8 +23,13 @@ const Comments = ({ comment }: { comment: CommentProps }) => {
         <Avatar alt="wale" radius="md" src={comment.madeBy.profileimage} />
         <Stack spacing="sm" className={classes.comments}>
           <Group align="center">
-            <Text fz="0.85rem" fw={500}>
-              {comment.madeBy.username}
+            <Text
+              fz="0.85rem"
+              fw={500}
+              component={Link}
+              to={`/${comment.madeBy.username}`}
+            >
+              {comment.madeBy.name}
             </Text>
             <Text fz="0.65rem" fw={500} c="dimmed">
               {formattedDate}
